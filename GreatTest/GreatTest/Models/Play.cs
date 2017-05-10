46c9b7319ef4e65de6f58c05cc98b29fdd76b0f3
ampii <amanda@balaz.se>
2017-05-10 16:46:41 +0200

    lagt till vy och model samt påbörjat javascript för att svara på fråga

---
+++ GreatTest/GreatTest/Models/Play.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreatTest.Models
+{
+    public class Play
+    {
+        public int QuestId { get; set; }
+        public string Quest { get; set; }
+        public string AnsA { get; set; }
+        public string AnsB { get; set; }
+        public string AnsC { get; set; }
+        public string AnsD { get; set; }
+        public string CorAns { get; set; }
+    }
+}
